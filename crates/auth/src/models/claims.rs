b9c4118ea6468e4055714b6c7d08e1d96a35a750
extr4d <extr4ditad0@proton.me>
2024-12-19 18:16:57 -0400

    feat(auth): token struct

--- crates/auth/src/models/claims.rs
+++ crates/auth/src/models/claims.rs
@@ -7,7 +7,26 @@ use super::{
     roles::Role,
 };
 
+/// The audience of the token.
+const JWT_AUDIENCE: &str = "akira";
+/// The duration of the token.
 const TOKEN_DURATION: Duration = Duration::days(1);
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub(crate) raw: String,
+    pub(crate) claims: Claims,
+}
+
+impl Token {
+    pub fn uid(&self) -> Uuid {
+        self.claims.uid
+    }
+
+    pub fn raw(&self) -> &str {
+        &self.raw
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Claims {
@@ -25,25 +44,40 @@ impl Claims {
             iat: Utc::now().timestamp(),
             role: role.clone(),
             permissions: role.permissions(),
-            exp: (Utc::now() + TOKEN_DURATION).timestamp(),
+            exp: Utc::now()
+                .checked_add_signed(TOKEN_DURATION)
+                .unwrap()
+                .timestamp(),
         }
     }
 
-    pub fn encode(&self, secret: &str) -> Result<String, jsonwebtoken::errors::Error> {
-        jsonwebtoken::encode(
+    pub fn encode(&self, secret: &str) -> Result<Token, jsonwebtoken::errors::Error> {
+        let token = jsonwebtoken::encode(
             &jsonwebtoken::Header::default(),
             self,
             &jsonwebtoken::EncodingKey::from_secret(secret.as_ref()),
-        )
+        )?;
+
+        Ok(Token {
+            raw: token,
+            claims: self.clone(),
+        })
     }
 
-    pub fn decode(token: &str, secret: &str) -> Result<Self, jsonwebtoken::errors::Error> {
-        jsonwebtoken::decode::<Claims>(
-            token,
+    pub fn decode(token: String, secret: &str) -> Result<Token, jsonwebtoken::errors::Error> {
+        let mut validation = jsonwebtoken::Validation::default();
+        validation.set_audience(JWT_AUDIENCE.as_bytes());
+
+        let decode = jsonwebtoken::decode::<Claims>(
+            &token,
             &jsonwebtoken::DecodingKey::from_secret(secret.as_ref()),
-            &jsonwebtoken::Validation::default(),
-        )
-        .map(|data| data.claims)
+            &validation,
+        )?;
+
+        Ok(Token {
+            raw: token,
+            claims: decode.claims,
+        })
     }
 
     pub fn validate(&self) -> bool {
